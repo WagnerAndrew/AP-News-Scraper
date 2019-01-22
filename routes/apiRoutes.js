97191e22a556d594bddedd6f1cd0c09bb40e3d3c
WagnerAndy <wagner.andrew.james@gmail.com>
2019-01-21 19:11:30 -0700

    Full app

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ var db = require("../models");
 
 module.exports = function (app) {
 
-// SCRAPE START//////////////////////////////////////////////////////////////////////////////////////////////////////
+  // SCRAPE START//////////////////////////////////////////////////////////////////////////////////////////////////////
   app.get("/scrape", function (req, res) {
 
     axios.get("https://www.apnews.com/apf-topnews").then(function (response) {
@@ -19,11 +19,8 @@ module.exports = function (app) {
         article.summary = $(element).find("a.content-container").find("div.content").find("p").text();
         article.link = $(element).find("div.CardHeadline").find("a.headline").attr("href");
 
-        // console.log("Article looks like this: ", article);
-
         db.Article.create(article)
           .then(function (dbArticle) {
-            // console.log("Articles created: ", dbArticle);
           })
           .catch(function (err) {
             console.log(err);
@@ -32,8 +29,6 @@ module.exports = function (app) {
 
       db.Article.find({})
         .then(function (dbArticle) {
-          // console.log("dbArticle is: ", dbArticle);
-
           res.render("index", {
             article: dbArticle
           });
@@ -43,136 +38,92 @@ module.exports = function (app) {
         });
     });
   });
-// SCRAPE END//////////////////////////////////////////////////////////////////////////////////////////////////////
+  // SCRAPE END//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
-
-// SAVE ARTICLE START//////////////////////////////////////////////////////////////////////////////////////////////
+  // SAVE ARTICLE START//////////////////////////////////////////////////////////////////////////////////////////////
   app.get("/saveArticle/:id", function (req, res) {
-
-    // console.log("req.params is: ", req.params.id );
 
     db.Article.update(req.body)
       .then(function (dbArticle) {
         return db.Article.findOneAndUpdate({ _id: req.params.id }, { $set: { saved: true } });
       })
       .then(function (dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
         res.json(dbArticle);
       })
       .catch(function (err) {
-        // If an error occurred, send it to the client
         res.json(err);
       });
   });
-// SAVE ARTICLE END////////////////////////////////////////////////////////////////////////////////////////////////
+  // SAVE ARTICLE END////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// DELETE ARTICLE START//////////////////////////////////////////////////////////////////////////////////////////////
-app.get("/deleteArticle/:id", function (req, res) {
+  // DELETE ARTICLE START//////////////////////////////////////////////////////////////////////////////////////////////
+  app.get("/deleteArticle/:id", function (req, res) {
 
-  db.Article.update(req.body)
-  .then(function (dbArticle) {
-    return db.Article.findOneAndUpdate({ _id: req.params.id }, { $set: { saved: false } });
-  })
-  .then(function (dbArticle) {
-    res.json(dbArticle);
-    
-  })
-  .catch(function (err) {
-    res.json(err);
+    db.Article.update(req.body)
+      .then(function (dbArticle) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { $set: { saved: false } });
+      })
+      .then(function (dbArticle) {
+        res.json(dbArticle);
+
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
   });
-});
-// DELETE ARTICLE END////////////////////////////////////////////////////////////////////////////////////////////////
+  // DELETE ARTICLE END////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+  // NOTE POST START//////////////////////////////////////////////////////////////////////////////////////////////
+  app.post("/saveNote/:id", function (req, res) {
 
-// NOTE POST START//////////////////////////////////////////////////////////////////////////////////////////////
-app.post("/saveNote/:id", function(req, res) {
-  
-  var newNote = {
-    articleID : req.params.id,
-    body : req.body.note
-  };
+    var newNote = {
+      articleID: req.params.id,
+      body: req.body.note
+    };
 
-  db.Note.create(newNote)
-    .then(function(dbNote) {
-      console.log("dbNote is: ", dbNote);
-      res.json(dbNote);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-// NOTE POST END////////////////////////////////////////////////////////////////////////////////////////////////
-
-// NOTES GATHER END////////////////////////////////////////////////////////////////////////////////////////////////
-app.get("/notes/:id", function(req, res) {
-  db.Note.find({ articleID: req.params.id })
-    .then(function(dbNote) {
-      res.json(dbNote)
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-// NOTES GATHER END////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-// DELETE ARTICLE START//////////////////////////////////////////////////////////////////////////////////////////////
-app.get("/deleteNote/:id", function (req, res) {
-
-  db.Note.remove({ _id: req.params.id })
-  .then(function (dbNote) {
-   
-    res.json(dbNote);
-    
-  })
-  .catch(function (err) {
-    res.json(err);
+    db.Note.create(newNote)
+      .then(function (dbNote) {
+        console.log("dbNote is: ", dbNote);
+        res.json(dbNote);
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
   });
-});
-// DELETE ARTICLE END////////////////////////////////////////////////////////////////////////////////////////////////
+  // NOTE POST END////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-//Close Exports
+  // NOTES GATHER END////////////////////////////////////////////////////////////////////////////////////////////////
+  app.get("/notes/:id", function (req, res) {
+    db.Note.find({ articleID: req.params.id })
+      .then(function (dbNote) {
+        res.json(dbNote)
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
+  // NOTES GATHER END////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  // DELETE ARTICLE START//////////////////////////////////////////////////////////////////////////////////////////////
+  app.get("/deleteNote/:id", function (req, res) {
+
+    db.Note.remove({ _id: req.params.id })
+      .then(function (dbNote) {
+
+        res.json(dbNote);
+
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
+  // DELETE ARTICLE END////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  //Close Exports
 };
-
-
-
-
-
-// // NOTES GATHER END////////////////////////////////////////////////////////////////////////////////////////////////
-// app.get("/articles/:id", function(req, res) {
-//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//   db.Article.findOne({ _id: req.params.id })
-//     // ..and populate all of the notes associated with it
-//     .populate("note")
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find an Article with the given id, send it back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
-// // NOTES GATHER END////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// app.get("/articles", function (req, res) {
-//   // Grab every document in the Articles collection
-//   db.Article.find({})
-//     .then(function (dbArticle) {
-//       // If we were able to successfully find Articles, send them back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function (err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,34 +1,33 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-// Load index page
-app.get("/", function(req, res) {
+  // Load index page
+  app.get("/", function (req, res) {
     res.render("index")
-});
+  });
 
-// Load index page
-app.get("/index", function(req, res) {
+  // Load index page
+  app.get("/index", function (req, res) {
     res.render("index")
-});
+  });
 
-// Load saved articles
-app.get("/savedArticles", function(req, res) {
+  // Load saved articles
+  app.get("/savedArticles", function (req, res) {
 
     db.Article.find({ saved: true })
-    .then(function (dbArticle) {
-      // console.log("dbArticle is: ", dbArticle);
-      res.render("savedArticles", {
-        article: dbArticle
+      .then(function (dbArticle) {
+
+        res.render("savedArticles", {
+          article: dbArticle
+        });
+      })
+      .catch(function (err) {
+        console.log(err);
       });
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
 
-});
+  });
 
 
-
-//Close export
+  //Close export
 };
