189d428cc09b418293beb608df4cbd080d8a9558
WagnerAndy <wagner.andrew.james@gmail.com>
2019-01-21 19:26:07 -0700

    Deployment stage

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ var exphbs = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-var db = require("./models");
-
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -17,8 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/scraperASSIGNMENT", { useNewUrlParser: true });
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraperASSIGNMENT";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraperASSIGNMENT";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Handlebars
 app.engine(
@@ -30,7 +25,6 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
